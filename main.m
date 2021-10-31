f47c865e75516b46bfc20bc1c17ca4891054ceb7
Nicola-Fonzi <nicola.fonzi@polimi.it>
2021-10-31 17:43:07 +0100

    First modifications to main

--- main.m
+++ main.m
@@ -4,125 +4,98 @@ clc
 
 %% General inputs
 
-% INPUT FILE
-filename_sma = 'D:\Desktop\PhD\X-DIA\ElevatorOnlyLCO\numericalSimulations\Final_DuringGVT_plate\NeoCASS\flutter.dat';
+% Input file
+filename_sma = 'D:\Desktop\PhD\X-DIA\ElevatorOnlyLCO\numericalSimulations\Final_DuringGVT_plate\correlated\steelWeightSteelConnection\NeoCASS\flutter.dat';
 
-% SOLVER INFO
+% Solver info
 ver = get_neolco_version(true);
 
-% ADDITIONAL VALUES FOR THE MODEL ANALYSIS
+% Model specific values
 tipLE = 40002;
 tipTE = 40001;
 midspanLE = 40006;
 midspanElevator = 40015;
 hingeScalarPoint = 1;
-
-% INPUTS FOR THE DESCRIBING FUNCTION
 kNominal = 48;
+airDensity = 1.13;
+
+% Inputs for the definition of the common base
+optimalBaseOptions.nModes = [8 12 16 20];
+optimalBaseOptions.fictitiousMass = [1e-4 1e-2 1e0 1e2 1e4];
+optimalBaseOptions.stiffnessFactor = [0.00001 0.001 0.1 0.5 1];
+optimalBaseOptions.optimumKnown = false;
+optimalBaseOptions.gapPoints = {hingeScalarPoint,'s',0,'Elevator hinge'};
+optimalBaseOptions.kNominal = {kNominal};
+
+% Inputs for the aero database
+aeroDatabaseOptions.selectionTrim = 'all';
+aeroDatabaseOptions.DynVLM = false;
+aeroDatabaseOptions.DynVLMtype = 'unsteady';
+
+% Inputs for the time marching approach
+preprocessTimeMarchingOptions.DynVLM = false;
+preprocessTimeMarchingOptions.DynVLMtype = aeroDatabaseOptions;
+preprocessTimeMarchingOptions.selectionTrim = 1;
+preprocessTimeMarchingOptions.simulinkModel = 'SISO';
+preprocessTimeMarchingOptions.gapPoints = optimalBaseOptions.gapPoints;
+preprocessTimeMarchingOptions.gapBehaviour = {'freeplay','static',''};
+preprocessTimeMarchingOptions.monitorPoints = {tipLE,'g',3,'Accelerometer at the tip LE';...
+                                               tipTE,'g',3,'Accelerometer at the tip TE';...
+                                               midspanLE,'g',3,'Accelerometer at midpan LE';...
+                                               midspanElevator,'g',3,'Accelerometer at midpan elevator'};                                           
+timeMarchingOptions.gap = {[0.1, 0.2, 0.4, 0.6, 1, 1.5, 2, 2.5, 3]/180*pi};
+timeMarchingOptions.kNominal = optimalBaseOptions.kNominal;
+timeMarchingOptions.rho = airDensity;
+timeMarchingOptions.speedVector = (14:2:50);
+timeMarchingOptions.speedBehaviour = 'both'; 
+timeMarchingOptions.speedInterpolationType = 0;
+timeMarchingOptions.nFFTwindows = length(timeMarchingOptions.speedVector);
+timeMarchingOptions.FFTwindowLength = 20;
+timeMarchingOptions.overlapWindows = -10;
+timeMarchingOptions.initialisationTime = 10;
+timeMarchingOptions.amplitudeDefinition = 'std';
+timeMarchingOptions.introduceFlightLoads = false;
+timeMarchingOptions.trimType = 'grounded';
+timeMarchingOptions.selectionTrim = 1;
+
+% Inputs for the describing function approach
+recomputeBase = false;
+searchFlutterQuenchingPoint = true;
 maxKeq = 1.8;
 maxNumberKeq = 25;
 flutterOptions.Vmax = 70;
 flutterOptions.Vmin = 5;
 flutterOptions.Vstep = 0.5;
 flutterOptions.method = 'PK0';
-flutterOptions.rho = 1.13;
+flutterOptions.rho = airDensity;
 modesToPlotDF = 1:8;
-
-% INPUT FOR THE TIME MARCHING
-preprocessTimeMarchingOptions.simulinkModel = 'SISO';
-preprocessTimeMarchingOptions.gapPoints = {hingeScalarPoint,'s',0,'Elevator hinge'};
-preprocessTimeMarchingOptions.gapBehaviour = {'freeplay','static',''}; % Unfortunately, the simulink model has to be built around the definition of static or dynamic gap, it cannot be decided later on.
-preprocessTimeMarchingOptions.monitorPoints = {tipLE,'g',3,'Accelerometer at the tip LE';...
-                                               tipTE,'g',3,'Accelerometer at the tip TE';...
-                                               midspanLE,'g',3,'Accelerometer at midpan LE';...
-                                               midspanElevator,'g',3,'Accelerometer at midpan elevator'};
-                                           
-timeMarchingOptions.gap = {[0.1, 0.2, 0.4, 0.6, 1, 1.5, 2, 2.5, 3]/180*pi};
-timeMarchingOptions.kNominal = {kNominal};   % In the MIMO case this will be a vector
-timeMarchingOptions.rho = flutterOptions.rho;
-timeMarchingOptions.speedVector = (14:2:50);
-timeMarchingOptions.speedBehaviour = 'both'; % The behaviour of speed can be easily changed without rebuilding the model
-timeMarchingOptions.speedInterpolationType = 0;
-timeMarchingOptions.nFFTwindows = length(timeMarchingOptions.speedVector);
-timeMarchingOptions.FFTwindowLength = 20;
-timeMarchingOptions.overlapWindows = -10;
-timeMarchingOptions.initialisationTime = 10;
-timeMarchingoptions.amplitudeDefinition = 'std';
-timeMarchingoptions.introduceFlightLoads = false;
-timeMarchingoptions.trimType = 'grounded';
-timeMarchingoptions.selectionTrim = 1;
-
-% FLAGS DEFINING THE BEHAVIOUR OF THE CODE
-recomputeBase = false;
-searchFlutterQuenchingPoint = true;
-optimalBaseOptions.NMODES = [8 12 16 20];
-optimalBaseOptions.FM = [1e-4 1e-2 1e0 1e2 1e4];
-optimalBaseOptions.KN = [0.00001 0.4 0.8 1.2 5 kNominal];
-optimalBaseOptions.OptimumKnown = true;
-optimalBaseOptions.FICTMASS_CMASS_ID = 34006;
-HINGE_CELAS_ID = 34106;
-optimalBaseOptions.HINGE_CELAS_ID = HINGE_CELAS_ID;
-
-
-%% Loading of the models
-inputData = readSmartcadFile(filename_sma);
-
-inputData_stiff = inputData;
-inputData_stiff.CELAS.ID = [inputData_stiff.CELAS.ID HINGE_CELAS_ID];
-inputData_stiff.CELAS.value = [inputData_stiff.CELAS.value kNominal];
-inputData_stiff.CELAS.Node = [inputData_stiff.CELAS.Node [hingeScalarPoint;0]];
-inputData_stiff.CELAS.DOF = [inputData_stiff.CELAS.DOF [0;0]];
-inputData_stiff.CELAS.PID = [inputData_stiff.CELAS.PID 0];
-
-[model, options] = processInputData(inputData);
-
-[model_stiff, options_stiff] = processInputData(inputData_stiff);
-
-%% Gather missing options from the model
-struOpt = [];
-
-eigOpt = options.eig;
-
 flutterOptions.axesUsed = 'body';
-flutterOptions.Mlist_dlm = options.aero.Mlist_dlm;
-flutterOptions.klist = options.aero.klist;
 
-fid = options.FID;
+%% Loading of the model
 
-%% Preprocessing
-
-struData = structuralPreprocessor(fid, model, struOpt);
-
-struData_stiff = structuralPreprocessor(fid, model_stiff, struOpt);
-
-%% Recover of the DOF indeces required for postprocessing
-
-[IDtable, gridDOF, spointDOF] = getIDtable(model.Node.ID, model.Spoint.ID);
-
-HingePos = find(model.Spoint.ID == hingeScalarPoint,1);
-HingeDOF = spointDOF(HingePos);
-
+inputData = readSmartcadFile(filename_sma);
 
 %% Generate common basis
 
-reducedBasis = [];
+[reducedBasis, struData, model, struData_stiff, model_stiff, resultsFolder, options] = obtainOptimalBase(inputData, optimalBaseOptions);
 
-if recomputeBase==0
+reducedBasis.Bmm = modalDamp(model, reducedBasis.V'*struData.Mzz*reducedBasis.V, reducedBasis.V'*struData.Kzz*reducedBasis.V,'Real');
 
-    [reducedBasis, resultsFolder] = obtainOptimalBase(inputData, model, struData, options, model_stiff, struData_stiff, optimalBaseOptions, HingeDOF);
+mkdir(resultsFolder)
+chdir(resultsFolder)
 
-    reducedBasis.Bmm = modalDamp(model, reducedBasis.V'*struData.Mzz*reducedBasis.V, reducedBasis.V'*struData.Kzz*reducedBasis.V,'Real');
+%% Generate aerodynamic database
 
-    mkdir("TimeMarching")
-    chdir("TimeMarching")
-
-    mkdir(resultsFolder)
-    chdir(resultsFolder)
-
-end
-
+[aeroData, options] = buildFullAeroData(model, model_stiff, struData_stiff, options, aeroDatabaseOptions);
 
 %% Different equivalent stifnesses
+
+flutterOptions.Mlist_dlm = options.aero.Mlist_dlm;
+flutterOptions.klist = options.aero.klist;
+
+if recomputeBase
+    reducedBasis = [];
+end
 
 [flutterSpeed, flutterFrequency, KeqVect, resultsFlutter, aeroData] = describingFunctionsPK(maxKeq, maxNumberKeq, inputData, HINGE_CELAS_ID, hingeScalarPoint, struOpt, fid, eigOpt, ...
     recomputeBase, flutterOptions, reducedBasis, searchFlutterQuenchingPoint, modesToPlotDF);
@@ -130,7 +103,6 @@ end
 if recomputeBase
     return
 end
-
 
 %% Here we try to reproduce the same using a time marching simulation
 
@@ -248,10 +220,8 @@ for i = 1:size(timeMarchingResults.stiffnessCombinations,2)
         for y = 1:size(toPlotUp,1)
             figure(y+100)
             hold on
-            plotHysteresis(timeMarchingOptions.speedVector,toPlotUp(y,:)./...
-                timeMarchingResults.gapCombinations(:,j)*2,'LineWidth',1.5,'color',colorTable(iColor,:))
-            plotHysteresis(timeMarchingOptions.speedVector,toPlotDown(y,:)./...
-                timeMarchingResults.gapCombinations(:,j)*2,'LineWidth',1.5,'color',colorTable(iColor,:))
+            plotHysteresis(timeMarchingOptions.speedVector,toPlotUp(y,:),'LineWidth',1.5,'color',colorTable(iColor,:))
+            plotHysteresis(timeMarchingOptions.speedVector,toPlotDown(y,:),'LineWidth',1.5,'color',colorTable(iColor,:))
         end
         iColor = iColor+1;
         legendTitle{index} = ['Gap ',num2str(timeMarchingResults.gapCombinations(:,j).'),' Stiffness ',num2str(timeMarchingResults.stiffnessCombinations(:,i).'),' TM UpperLimit'] ;
